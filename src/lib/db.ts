7037df573418341d44dd087b69569f67186f98b5
yashpatil641 <yashpatil4395@gmail.com>
2025-05-25 02:37:11 +0530

    feat: Add new dependencies for Neon database and Prisma adapter

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,9 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['query'],
+})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
